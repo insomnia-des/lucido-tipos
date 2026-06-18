d13c2a3089a27afb28a0a10931e74ed8b4c0181e
anfigeno <dolorcriticodevastador@proton.me>
2026-06-18 01:55:44 -0500

    Mudamos a Valibot

--- src/bd/Usuario.ts
+++ src/bd/Usuario.ts
@@ -1,7 +1,8 @@
 import type { Insertable, Selectable, Updateable } from "kysely";
-import z from "zod";
+import * as v from "valibot";
 import type { TablaBase } from "./TablaBase";
-import { esquemaZod } from "../util";
+import { type EsquemaPara } from "../util";
+import { idDeDiscord } from "../esquemasComunes";
 
 export interface TablaUsuario extends TablaBase {
   id_de_discord: string;
@@ -12,19 +13,11 @@ export type Usuario = Selectable<TablaUsuario>;
 export type CrearUsuario = Insertable<TablaUsuario>;
 export type ActualizarUsuario = Updateable<TablaUsuario>;
 
-export const EsquemaCrearUsuario = esquemaZod<CrearUsuario>()(
-  z.object({
-    id_de_discord: z
-      .string("Debe ser una cadena")
-      .min(18, "Debe tener al menos 18 caracteres")
-      .max(20, "Debe tener máximo 20 caracteres"),
-    nombre: z
-      .string("Debe ser una cadena")
-      .min(2, "Debe tener al menos 2 caracteres")
-      .max(32, "Debe tener máximo 32 caracteres"),
-  }),
-);
+export const EsquemaCrearUsuario = v.object({
+  id_de_discord: idDeDiscord,
+  nombre: v.pipe(v.string(), v.minLength(2), v.maxLength(32)),
+}) satisfies EsquemaPara<CrearUsuario>;
 
-export const EsquemaActualizarUsuario = esquemaZod<ActualizarUsuario>()(
-  EsquemaCrearUsuario.partial(),
-);
+export const EsquemaActualizarUsuario = v.partial(
+  EsquemaCrearUsuario,
+) satisfies EsquemaPara<ActualizarUsuario>;
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,3 @@
 import type { GenericSchema } from "valibot";
-import type z from "zod";
-
-export function esquemaZod<T>() {
-  return <S extends z.ZodType<T>>(esquema: S): S => esquema;
-}
 
 export type EsquemaPara<T> = GenericSchema<unknown, T>;
