6824fd07c475781fbd2162ceb0fd02116d17ea87
anfigeno <dolorcriticodevastador@proton.me>
2026-06-19 14:18:56 -0500

    Reemplazamos el tipo Date por string

--- src/bd/TablaBase.ts
+++ src/bd/TablaBase.ts
@@ -2,6 +2,6 @@ import type { ColumnType } from "kysely";
 
 export interface TablaBase {
   id: ColumnType<number, never, never>;
-  creado_en: ColumnType<Date, never, never>;
-  actualizado_en: ColumnType<Date, never, never>;
+  creado_en: ColumnType<string, never, never>;
+  actualizado_en: ColumnType<string, never, never>;
 }
